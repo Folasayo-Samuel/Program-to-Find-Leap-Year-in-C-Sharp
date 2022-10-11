8ad410a50d7710437ba63dbec2841a126baa646a
Folasayo Samuel <folasayosamuel@gmail.com>
2022-10-11 15:36:43 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgramToFindLeapYear
+{
+	public class Program
+	{
+		public static void Main(string[]args)
+		{
+			int year;
+			Console.Write("Enter the year: ");
+			year = Convert.ToInt32(Console.ReadLine());
+			if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+				Console.WriteLine($"{year} is Leap Year");
+			else
+				Console.WriteLine($"{year} is not a Leap Year");
+				
+			Console.ReadLine();
+			
+		}
+	}
+}
